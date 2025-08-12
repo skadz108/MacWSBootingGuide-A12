d52c73d1860bcd7eb82723447ec14cc32c886220
Skadz <46767289+skadz108@users.noreply.github.com>
2025-08-11 18:15:25 -0700

    i should prolly change this

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ set -e
 
 DEVICE_IP="iphone-cua-duy.local"
 
-gmake FINALPACKAGE=1 STRIP=0 THEOS_PACKAGE_SCHEME=rootless package install THEOS_DEVICE_IP=$DEVICE_IP THEOS_DEVICE_PORT=2222 GO_EASY_ON_ME=1
+gmake FINALPACKAGE=1 STRIP=0 package install THEOS_DEVICE_IP=$DEVICE_IP THEOS_DEVICE_PORT=2222 GO_EASY_ON_ME=1
 
 cp .theos/obj/libmachook.dylib .
 #vtool -set-build-version 1 11.0 11.0 -replace -output libmachook.dylib .theos/obj/libmachook.dylib
@@ -11,9 +11,9 @@ cp .theos/obj/libmachook.dylib .
 vtool -arch arm64 -set-build-version 1 13.0 13.0 -replace -output .theos/obj/launchservicesd .theos/obj/launchservicesd
 ldid -S -M .theos/obj/launchservicesd
 
-libmachook_path="/var/jb/usr/macOS/lib/libmachook.dylib"
-driverhost_path="/var/jb/usr/macOS/Frameworks/MTLSimDriver.framework/XPCServices/MTLSimDriverHost.xpc/MTLSimDriverHost"
-launchdchrootexec_path="/var/jb/usr/macOS/LaunchDaemons/launchdchrootexec"
+libmachook_path="/usr/macOS/lib/libmachook.dylib"
+driverhost_path="/usr/macOS/Frameworks/MTLSimDriver.framework/XPCServices/MTLSimDriverHost.xpc/MTLSimDriverHost"
+launchdchrootexec_path="/usr/macOS/LaunchDaemons/launchdchrootexec"
 driverhost_hash=$(ldid -h .theos/obj/MTLSimDriverHost | grep CDHash= | cut -c8-)
 libmachook_hash=$(ldid -h libmachook.dylib | grep CDHash= | cut -c8-)
 exec_hash=$(ldid -h .theos/obj/exec | grep CDHash= | cut -c8-)
